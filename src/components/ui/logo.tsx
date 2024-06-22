e1b8a78a96ddac9b56378f2b3bf6f13ef75da5ae
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-06-22 21:17:35 +0900

    header 로고 크기 다른 부분 수정

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import TextLogo from "./text_logo";
 
-//로고 컴포넌트
-interface Props {
-  size?: "s" | "b";
-}
-
-export default function Logo({ size = "s" }: Props) {
-  const textSize = size === "s" ? "text-3xl" : "text-6xl";
+export default function Logo() {
   return (
     <Link href={"/"}>
       <h1
-        className={`flex items-center gap-2 text-point_color ${textSize} font-bold cursor-pointer`}>
-        <Image src={"/images/logo.png"} alt="logo" width={38} height={38} />
+        className={`flex items-center gap-2 text-point_color font-bold cursor-pointer`}>
+        <Image
+          className="w-[28px] h-[28px]"
+          src={"/images/logo.png"}
+          alt="logo"
+          width={38}
+          height={38}
+        />
         <TextLogo size="s" />
       </h1>
     </Link>
